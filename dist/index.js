306fcd4636524758f348ca355aeda5975ec2dc45
Ayush <thapaaayush115@gmail.com>
2022-11-17 15:23:29 +0545

    font size increased

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "BlueButton", {
+  enumerable: true,
+  get: function get() {
+    return _BlueButton.default;
+  }
+});
+var _BlueButton = _interopRequireDefault(require("./components/BlueButton"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/lib/components/BlueButton.jsx
+++ src/lib/components/BlueButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BlueButton() {
     return (
         <div>
-            <button style={{ color: "white", backgroundColor: "blue" }}>
+            <button style={{ color: "white", backgroundColor: "blue", fontSize: "50px" }}>
                 Blue Button
             </button>
         </div>
